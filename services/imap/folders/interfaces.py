495721e93da0789f22d0a53b0104d4c28f437daf
Шулаков Иван <ivanshulakov9@gmail.com>
2026-08-17 12:42:41 +0300

    feat: add folder runner interface

--- services/imap/folders/interfaces.py
+++ services/imap/folders/interfaces.py
@@ -1,5 +1,9 @@
 import imaplib
 from abc import ABC, abstractmethod
+from pathlib import Path
+from typing import Protocol
+
+from tqdm import tqdm
 
 from dto.imap_folder import ImapFolder
 
@@ -15,3 +19,22 @@ class IImapFolderProvider(ABC):
 class IImapFolderParser(ABC):
     @abstractmethod
     def parse(self, raw_folder: bytes) -> ImapFolder: ...
+
+
+class IImapSyncFolderRunner(Protocol):
+    def run(
+        self,
+        folder: ImapFolder,
+        log_file: Path,
+        host1: str,
+        host2: str,
+        user1: str,
+        user2: str,
+        auth_user1: str,
+        auth_user2: str,
+        password1: str,
+        password2: str,
+        progress: tqdm,
+        port1: int = 993,
+        port2: int = 993,
+    ): ...
