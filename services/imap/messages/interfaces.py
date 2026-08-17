e98344216784f6413b193b9376f783b76ed75803
Шулаков Иван <ivanshulakov9@gmail.com>
2026-08-17 16:59:05 +0300

    feat: add message verifier interfaces

--- services/imap/messages/interfaces.py
+++ services/imap/messages/interfaces.py
@@ -1,13 +1,32 @@
 import imaplib
-from abc import ABC, abstractmethod
+from typing import Protocol
 
 from dto.imap_folder import ImapFolder
+from services.imap.auth.interfaces import IImapAuthenticator
+from services.imap.folders.interfaces import IImapFolderProvider
 
 
-class IImapMessageCounter(ABC):
-    @abstractmethod
+class IImapMessageCounter(Protocol):
     def get_count(
         self,
         imap: imaplib.IMAP4_SSL,
         folder: ImapFolder,
     ) -> int: ...
+
+
+class IImapMessagesComparer(Protocol):
+    def compare(
+        self,
+        source_imap: imaplib.IMAP4_SSL,
+        targe_imap: imaplib.IMAP4_SSL,
+    ) -> bool: ...
+
+
+class IImapMessageVerifier(Protocol):
+    def verify(
+        self,
+        source_folders: list[ImapFolder],
+        target_authenticator: IImapAuthenticator,
+        target_provider: IImapFolderProvider,
+        target_imap: imaplib.IMAP4_SSL,
+    ) -> bool: ...
