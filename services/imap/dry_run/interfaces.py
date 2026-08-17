b63fb4c8c9af602ade1a6c3d08e1b841cc2ecd3b
Шулаков Иван <ivanshulakov9@gmail.com>
2026-08-17 12:41:56 +0300

    feat: add dry run interface

---
+++ services/imap/dry_run/interfaces.py
@@ -0,0 +1,17 @@
+from typing import Protocol
+
+
+class IImapSyncDryRun(Protocol):
+    def dry_run(
+        self,
+        host1: str,
+        host2: str,
+        user1: str,
+        user2: str,
+        auth_user1: str,
+        auth_user2: str,
+        password1: str,
+        password2: str,
+        port1: int = 993,
+        port2: int = 993,
+    ) -> None: ...
