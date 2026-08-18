7ac059150afe449effb5635f081e5da53f51c418
Шулаков Иван <ivanshulakov9@gmail.com>
2026-08-18 09:50:39 +0300

    refactor: split main file into separate modules

---
+++ orchestration/parse_args.py
@@ -0,0 +1,35 @@
+import argparse
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description="IMAP mailbox migration",
+    )
+
+    parser.add_argument(
+        "current_user",
+        help="Исходный IMAP пользователь",
+    )
+
+    parser.add_argument(
+        "target_user",
+        help="Целевой IMAP пользователь",
+    )
+
+    parser.add_argument(
+        "--dry",
+        dest="enable_dry",
+        action="store_true",
+        default=False,
+        help="Включить dry-run",
+    )
+
+    parser.add_argument(
+        "--verify",
+        dest="enable_verify",
+        action="store_true",
+        default=False,
+        help="Включить проверку количества сообщений между серверами",
+    )
+
+    return parser.parse_args()
